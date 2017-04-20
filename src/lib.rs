fad452c2c62629c4d7efd51202d1ee2ff5dc78cd
cdouven <christian.douven@zalando.de>
2017-04-20 18:57:26 +0200

    moved code one level up - breaking change

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@
 //! Nakadion uses the [Subscription API](https://github.com/zalando/nakadi#subscriptions)
 //! of Nakadi on the consuming side.
 //!
-//! A producer might be added later.
-//!
 //! ## Documentation
 //!
 //! Documenatation can be found on [docs.rs](https://docs.rs)
@@ -46,11 +44,22 @@ extern crate error_chain;
 extern crate metrics as libmetrics;
 extern crate histogram;
 
-pub mod client;
+use std::sync::Arc;
+use std::thread::JoinHandle;
+
+use uuid::Uuid;
+
 mod tokenerrors;
 pub mod metrics;
+mod clienterrors;
+mod connector;
+mod worker;
 
 pub use tokenerrors::*;
+pub use clienterrors::*;
+pub use self::connector::{NakadiConnector, Checkpoints, ReadsStream, HyperClientConnector,
+                          ConnectorSettings, ConnectorSettingsBuilder};
+pub use self::worker::NakadiWorker;
 
 /// A token used for authentication against `Nakadi`.
 #[derive(Clone, Debug)]
@@ -81,5 +90,107 @@ impl EventType {
     /// [`EventType`](https://github.com/zalando/nakadi#creating-event-types).
     pub fn new<T: Into<String>>(value: T) -> EventType {
         EventType(value.into())
+    }
+}
+
+/// A `SubscriptionId` is used to guaratee a continous flow of events for a client.
+#[derive(Clone, Debug)]
+pub struct SubscriptionId(pub Uuid);
+
+impl SubscriptionId {
+    pub fn nil() -> Self {
+        SubscriptionId(Uuid::nil())
+    }
+}
+
+/// A `StreamId` identifies a subscription. It must be provided for checkpointing with a `Cursor`.
+#[derive(Clone, Debug)]
+pub struct StreamId(pub String);
+
+impl StreamId {
+    pub fn new<T: Into<String>>(id: T) -> Self {
+        StreamId(id.into())
+    }
+}
+
+/// A `Cursor` describes a position in the stream. The cursor is used for checkpointing.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Cursor {
+    pub partition: usize,
+    pub offset: String,
+    pub event_type: EventType,
+    pub cursor_token: Uuid,
+}
+
+pub struct BatchInfo {
+    pub stream_id: StreamId,
+    pub cursor: Cursor,
+}
+
+/// Describes what to do after a batch has been processed.
+///
+/// Use to control what should happen next.
+#[derive(Debug, Clone)]
+pub enum AfterBatchAction {
+    /// Checkpoint and get next
+    Continue,
+    /// Get next without checkpointing.
+    ContinueNoCheckpoint,
+    /// Checkpoint then stop.
+    Stop,
+    /// Stop without checkpointing
+    Abort,
+}
+
+/// Handles batches of events received from `Nakadi`.
+pub trait Handler: Send + Sync + 'static {
+    /// Handle the batch of events. The supplied string contains
+    /// the whole batch of events as a `JSOS` array.
+    /// Return an `AfterBatchAction` to tell what to do next.
+    /// The batch array may be empty.
+    /// You may not panic within the handler.
+    fn handle(&self, batch: &str, info: BatchInfo) -> AfterBatchAction;
+}
+
+/// The client to consume events from `Nakadi`
+pub struct NakadiClient<C: NakadiConnector> {
+    worker: NakadiWorker,
+    connector: Arc<C>,
+}
+
+impl<C: NakadiConnector> NakadiClient<C> {
+    /// Creates a new instance. The returned `JoinHandle` can
+    /// be used to synchronize with the underlying worker.
+    /// The underlying worker will be stopped once the client is dropped.
+    pub fn new<H: Handler>(subscription_id: SubscriptionId,
+                           connector: Arc<C>,
+                           handler: H)
+                           -> (Self, JoinHandle<()>) {
+        let (worker, handle) = NakadiWorker::new(connector.clone(), handler, subscription_id);
+        (NakadiClient {
+            worker: worker,
+            connector: connector,
+        },
+         handle)
+    }
+
+    /// Returns true if the underlying `NakadiWorker` is still running.
+    pub fn is_running(&self) -> bool {
+        self.worker.is_running()
+    }
+
+    /// Stop the underlying `NakadiWorker`.
+    pub fn stop(&self) {
+        self.worker.stop();
+    }
+
+    /// Get access to the underlying `NakadiConnector`.
+    pub fn connector(&self) -> &C {
+        &self.connector
+    }
+
+    /// Return the `SubscriptionId` this `NakadiClient` is listening to.
+    pub fn subscription_id(&self) -> &SubscriptionId {
+        self.worker.subscription_id()
     }
 }
