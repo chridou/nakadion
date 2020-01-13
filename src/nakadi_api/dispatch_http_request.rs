64de3a617e73a8d9451ea3f2cb4169490f128c0f
cdouven <christian.douven@zalando.de>
2020-01-13 18:29:50 +0100

    impl dispatch http request for reqwest

--- src/nakadi_api/dispatch_http_request.rs
+++ src/nakadi_api/dispatch_http_request.rs
@@ -2,50 +2,15 @@ use std::error::Error;
 use std::fmt;
 
 use bytes::Bytes;
-use futures::{future::BoxFuture, stream::BoxStream, FutureExt, TryFutureExt, TryStreamExt};
-use http::{Request, StatusCode};
-use http_api_problem::HttpApiProblem;
-use serde::de::DeserializeOwned;
-use serde_json;
+use futures::{future::BoxFuture, stream::BoxStream};
+use http::{Request, Response};
 
 pub type BytesStream<'a> = BoxStream<'a, Result<Bytes, IoError>>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ErrorExpectation {
-    /// Expect Nakadi to return a problem JSON
-    Problem,
-    /// Expect Nakadi to return a body that needs further evaluation
-    ContextualBody,
-}
-
-pub type ResponseFuture<'a> = BoxFuture<'a, Result<(StatusCode, BytesStream<'a>), RemoteCallError>>;
-pub type TypedResponseFuture<'a, T> = BoxFuture<'a, Result<(StatusCode, T), RemoteCallError>>;
+pub type ResponseFuture<'a> = BoxFuture<'a, Result<Response<BytesStream<'a>>, RemoteCallError>>;
 
 pub trait DispatchHttpRequest {
-    fn dispatch(&self, req: Request<Bytes>, error_expectation: ErrorExpectation) -> ResponseFuture;
-
-    fn dispatch_for_entity<T: DeserializeOwned>(
-        &self,
-        req: Request<Bytes>,
-        error_expectation: ErrorExpectation,
-    ) -> TypedResponseFuture<T>
-    where
-        Self: Sync,
-    {
-        async move {
-            let (status, mut bytes_stream) = self.dispatch(req, error_expectation).await?;
-
-            let mut all_bytes = Vec::new();
-
-            while let Some(next_bytes) = bytes_stream.try_next().await? {
-                all_bytes.extend(next_bytes);
-            }
-
-            let deserialized = serde_json::from_slice(&all_bytes[..])?;
-            Ok((status, deserialized))
-        }
-        .boxed()
-    }
+    fn dispatch(&self, req: Request<Bytes>) -> ResponseFuture;
 }
 
 #[derive(Debug)]
@@ -55,38 +20,29 @@ pub struct RemoteCallError {
     detail: RemoteCallErrorDetail,
 }
 
+#[derive(Debug)]
 pub struct IoError(pub String);
 
+impl fmt::Display for IoError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)?;
+
+        Ok(())
+    }
+}
+
+impl Error for IoError {
+    fn cause(&self) -> Option<&dyn Error> {
+        None
+    }
+}
+
 impl RemoteCallError {
-    pub fn new_problem<T: Into<HttpApiProblem>>(problem: T) -> Self {
-        Self {
-            message: None,
-            cause: None,
-            detail: RemoteCallErrorDetail::ResponseProblem(problem.into()),
-        }
-    }
-
-    pub fn new_http<S: Into<StatusCode>, B: Into<Bytes>>(status: S, body: B) -> Self {
-        Self {
-            message: None,
-            cause: None,
-            detail: RemoteCallErrorDetail::ResponseBody(status.into(), body.into()),
-        }
-    }
-
     pub fn new_io() -> Self {
         Self {
             message: None,
             cause: None,
             detail: RemoteCallErrorDetail::Io,
-        }
-    }
-
-    pub fn new_serialization() -> Self {
-        Self {
-            message: None,
-            cause: None,
-            detail: RemoteCallErrorDetail::Serialization,
         }
     }
 
@@ -108,36 +64,12 @@ impl RemoteCallError {
         self
     }
 
-    pub fn is_server(&self) -> bool {
-        self.detail.is_server()
-    }
-
-    pub fn is_client(&self) -> bool {
-        self.detail.is_client()
-    }
-
-    pub fn is_serialization(&self) -> bool {
-        self.detail.is_serialization()
-    }
-
     pub fn is_io(&self) -> bool {
         self.detail.is_io()
     }
 
     pub fn is_other(&self) -> bool {
         self.detail.is_other()
-    }
-
-    pub fn status_code(&self) -> Option<StatusCode> {
-        self.detail.status_code()
-    }
-
-    pub fn problem(&self) -> Option<&HttpApiProblem> {
-        self.detail.problem()
-    }
-
-    pub fn response_body(&self) -> Option<&[u8]> {
-        self.detail.response_body()
     }
 
     pub fn is_retry_suggested(&self) -> bool {
@@ -150,11 +82,6 @@ pub type RemoteCallResult<T> = Result<T, RemoteCallError>;
 impl fmt::Display for RemoteCallError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.detail {
-            RemoteCallErrorDetail::ResponseProblem(_) => {}
-            RemoteCallErrorDetail::ResponseBody(_, _) => {}
-            RemoteCallErrorDetail::Serialization => {
-                write!(f, "serialization error")?;
-            }
             RemoteCallErrorDetail::Io => {
                 write!(f, "io error")?;
             }
@@ -163,14 +90,8 @@ impl fmt::Display for RemoteCallError {
             }
         }
 
-        if let Some(status_code) = self.status_code() {
-            write!(f, " - status: {}", status_code)?;
-        }
-
         if let Some(ref message) = self.message {
             write!(f, " - message: {}", message)?;
-        } else if let Some(detail) = self.problem().and_then(|p| p.detail.as_ref()) {
-            write!(f, " - message: {}", detail)?;
         }
 
         Ok(())
@@ -183,18 +104,6 @@ impl Error for RemoteCallError {
     }
 }
 
-impl From<serde_json::Error> for RemoteCallError {
-    fn from(err: serde_json::Error) -> Self {
-        Self::new_serialization().with_cause(err)
-    }
-}
-
-impl From<HttpApiProblem> for RemoteCallError {
-    fn from(problem: HttpApiProblem) -> Self {
-        Self::new_problem(problem)
-    }
-}
-
 impl From<IoError> for RemoteCallError {
     fn from(err: IoError) -> Self {
         Self::new_io().with_message(err.0)
@@ -203,41 +112,11 @@ impl From<IoError> for RemoteCallError {
 
 #[derive(Debug)]
 enum RemoteCallErrorDetail {
-    ResponseProblem(HttpApiProblem),
-    ResponseBody(StatusCode, Bytes),
     Other,
-    Serialization,
     Io,
 }
 
 impl RemoteCallErrorDetail {
-    pub fn is_server(&self) -> bool {
-        match self {
-            RemoteCallErrorDetail::ResponseProblem(ref problem) => {
-                problem.status.map(|s| s.is_server_error()).unwrap_or(true)
-            }
-            RemoteCallErrorDetail::ResponseBody(status, _) => status.is_server_error(),
-            _ => false,
-        }
-    }
-
-    pub fn is_client(&self) -> bool {
-        match self {
-            RemoteCallErrorDetail::ResponseProblem(ref problem) => {
-                problem.status.map(|s| s.is_client_error()).unwrap_or(false)
-            }
-            RemoteCallErrorDetail::ResponseBody(status, _) => status.is_client_error(),
-            _ => false,
-        }
-    }
-
-    pub fn is_serialization(&self) -> bool {
-        match self {
-            RemoteCallErrorDetail::Serialization => true,
-            _ => false,
-        }
-    }
-
     pub fn is_io(&self) -> bool {
         match self {
             RemoteCallErrorDetail::Io => true,
@@ -252,36 +131,9 @@ impl RemoteCallErrorDetail {
         }
     }
 
-    pub fn status_code(&self) -> Option<StatusCode> {
-        match self {
-            RemoteCallErrorDetail::ResponseProblem(ref problem) => problem.status,
-            RemoteCallErrorDetail::ResponseBody(status, _) => Some(*status),
-            _ => None,
-        }
-    }
-
-    pub fn problem(&self) -> Option<&HttpApiProblem> {
-        match self {
-            RemoteCallErrorDetail::ResponseProblem(ref problem) => Some(problem),
-            _ => None,
-        }
-    }
-
-    pub fn response_body(&self) -> Option<&[u8]> {
-        match self {
-            RemoteCallErrorDetail::ResponseBody(_, ref body) => Some(&body),
-            _ => None,
-        }
-    }
-
     pub fn is_retry_suggested(&self) -> bool {
         match self {
-            RemoteCallErrorDetail::ResponseProblem(ref problem) => {
-                problem.status.map(|s| s.is_server_error()).unwrap_or(true)
-            }
-            RemoteCallErrorDetail::ResponseBody(status, _) => status.is_server_error(),
             RemoteCallErrorDetail::Other => false,
-            RemoteCallErrorDetail::Serialization => false,
             RemoteCallErrorDetail::Io => true,
         }
     }
@@ -289,7 +141,8 @@ impl RemoteCallErrorDetail {
 
 mod reqwest_dispatch_http_request {
     use futures::{stream::TryStreamExt, FutureExt, StreamExt};
-    use reqwest::{Client, Request as RRequest, Response};
+    use http::{Request, Response};
+    use reqwest::{Client, Request as RRequest};
 
     use super::*;
 
@@ -299,11 +152,7 @@ mod reqwest_dispatch_http_request {
     }
 
     impl DispatchHttpRequest for ReqwestDispatchHttpRequest {
-        fn dispatch(
-            &self,
-            req: Request<Bytes>,
-            error_expectation: ErrorExpectation,
-        ) -> ResponseFuture {
+        fn dispatch(&self, req: Request<Bytes>) -> ResponseFuture {
             async move {
                 let (parts, body) = req.into_parts();
 
@@ -323,51 +172,30 @@ mod reqwest_dispatch_http_request {
 
                 *request.body_mut() = Some(body.into());
 
-                let response = self.client.execute(request).await?;
-                evaluate_response(response, error_expectation).await
+                let reqwest_response = self.client.execute(request).await?;
+
+                let status = reqwest_response.status();
+                let headers = reqwest_response.headers().clone();
+                let version = reqwest_response.version();
+
+                let bytes_stream = reqwest_response
+                    .bytes_stream()
+                    .map_err(|err| IoError(err.to_string()))
+                    .boxed();
+                let mut response = Response::new(bytes_stream);
+
+                *response.status_mut() = status;
+                *response.headers_mut() = headers;
+                *response.version_mut() = version;
+
+                Ok(response)
             }
             .boxed()
         }
     }
 
-    async fn evaluate_response(
-        response: Response,
-        error_expectation: ErrorExpectation,
-    ) -> Result<(StatusCode, BytesStream<'static>), RemoteCallError> {
-        let status = response.status();
-        if status.is_success() {
-            let body_stream = response
-                .bytes_stream()
-                .map_err(|err| IoError(err.to_string()));
-            return Ok((status, body_stream.boxed()));
-        }
-
-        match error_expectation {
-            ErrorExpectation::ContextualBody => {
-                let bytes = response.bytes().await?;
-                Err(RemoteCallError::new_http(status, bytes))
-            }
-            ErrorExpectation::Problem => {
-                let bytes = response.bytes().await?;
-                let problem: HttpApiProblem =
-                    serde_json::from_slice(&bytes.to_vec()).map_err(|err| {
-                        RemoteCallError::new_http(status, bytes)
-                            .with_message("Expected an HTTP problem but failed to parse it")
-                            .with_cause(err)
-                    })?;
-                Err(RemoteCallError::new_problem(problem))
-            }
-        }
-    }
-
     impl From<reqwest::Error> for RemoteCallError {
         fn from(err: reqwest::Error) -> Self {
-            if let Some(status) = err.status() {
-                return RemoteCallError::new_problem(status)
-                    .with_message("Problem generated by Nakadion")
-                    .with_cause(err);
-            }
-
             if err.is_timeout() {
                 return RemoteCallError::new_io()
                     .with_message("Request timeout")
