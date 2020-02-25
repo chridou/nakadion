c1278583d9e65b41469784705081e6ba6aa79bb8
cdouven <christian.douven@zalando.de>
2020-02-25 17:53:40 +0100

    commit the test

--- nakadion/examples/complete_example.rs
+++ nakadion/examples/complete_example.rs
@@ -9,6 +9,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let nakadi_base_url = NakadiBaseUrl::from_env()?;
     let api_client = ApiClient::builder().finish_from_env()?;
 
+    println!("There should be no events");
     let known_event_types = api_client.list_event_types(RandomFlowId).await?;
     assert!(known_event_types.is_empty());
 
