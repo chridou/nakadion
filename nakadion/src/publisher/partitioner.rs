59c335dd353dd26bb95f0ffdfce52ab924b8fd77
cdouven <christian.douven@zalando.de>
2020-02-29 08:12:03 +0100

    doku

--- nakadion/src/publisher/partitioner.rs
+++ nakadion/src/publisher/partitioner.rs
@@ -1,3 +1,4 @@
+//! Tool for manually assigning partitions
 use std::hash::{BuildHasher, Hash, Hasher};
 
 use crc::crc64::{Digest, ISO};
@@ -12,6 +13,7 @@ use crate::nakadi_types::{
     RandomFlowId,
 };
 
+/// The default hasher used is [crc64::ISO](https://docs.rs/crc/1.9.0/crc/crc64/constant.ISO.html)
 #[derive(Clone)]
 pub struct DefaultBuildHasher;
 
@@ -23,6 +25,7 @@ impl BuildHasher for DefaultBuildHasher {
     }
 }
 
+/// Determines partitions based on hashes
 #[derive(Clone)]
 pub struct Partitioner<B: BuildHasher + Clone = DefaultBuildHasher> {
     partitions: Vec<PartitionId>,
@@ -30,14 +33,38 @@ pub struct Partitioner<B: BuildHasher + Clone = DefaultBuildHasher> {
 }
 
 impl Partitioner<DefaultBuildHasher> {
+    /// Create a new instance with the given partitions.
+    ///
+    /// The order of the given partitions will not be changed.
+    ///
+    /// ## Panics
+    ///
+    /// If partitions is empty
     pub fn new(partitions: Vec<PartitionId>) -> Self {
         Self::new_with_hasher(partitions, DefaultBuildHasher)
     }
 
+    /// Create a new instance with the given partitions.
+    ///
+    /// The partitions will be sorted by first trying to convert
+    /// the partitions to numbers and sorting by these. Otherwise
+    /// they will be sorted ba their contained string.
+    ///
+    /// ## Panics
+    ///
+    /// If partitions is empty
     pub fn new_sorted(partitions: Vec<PartitionId>) -> Self {
         Self::new_sorted_with_hasher(partitions, DefaultBuildHasher)
     }
 
+    /// Create a new instance for the partitions of the
+    /// given event type.
+    ///
+    /// The partitions will be sorted by first trying to convert
+    /// the partitions to numbers and sorting by these. Otherwise
+    /// they will be sorted ba their contained string.
+    ///
+    /// Fails if the event type has no partitions.
     pub async fn from_event_type<C>(
         event_type: &EventTypeName,
         api_client: C,
@@ -53,6 +80,14 @@ impl<B> Partitioner<B>
 where
     B: BuildHasher + Clone,
 {
+    /// Create a new instance with the given partitions and a provided
+    /// hashing algorithm.
+    ///
+    /// The order of the given partitions will not be changed.
+    ///
+    /// ## Panics
+    ///
+    /// If partitions is empty
     pub fn new_with_hasher(partitions: Vec<PartitionId>, build_hasher: B) -> Self {
         assert!(!partitions.is_empty(), "partitions may not be empty");
         Self {
@@ -61,11 +96,28 @@ where
         }
     }
 
-    pub fn new_sorted_with_hasher(mut partitions: Vec<PartitionId>, build_hasher: B) -> Self {
-        partitions.sort();
-        Self::new_with_hasher(partitions, build_hasher)
+    /// Create a new instance with the given partitions and a provided
+    /// hashing algorithm.
+    ///
+    /// The partitions will be sorted by first trying to convert
+    /// the partitions to numbers and sorting by these. Otherwise
+    /// they will be sorted ba their contained string.
+    ///
+    /// ## Panics
+    ///
+    /// If partitions is empty
+    pub fn new_sorted_with_hasher(partitions: Vec<PartitionId>, build_hasher: B) -> Self {
+        create_sorted_partitioner(partitions, build_hasher)
     }
 
+    /// Create a new instance for the partitions of the
+    /// given event type and a provided hashing algorithm.
+    ///
+    /// The partitions will be sorted by first trying to convert
+    /// the partitions to numbers and sorting by these. Otherwise
+    /// they will be sorted ba their contained string.
+    ///
+    /// Fails if the event type has no partitions.
     pub async fn from_event_type_with_hasher<C>(
         event_type: &EventTypeName,
         api_client: C,
@@ -85,26 +137,12 @@ where
             ));
         }
 
-        let partition_ids: Vec<PartitionId> = partitions.into_iter().map(|p| p.partition).collect();
+        let partitions: Vec<PartitionId> = partitions.into_iter().map(|p| p.partition).collect();
 
-        let ids_and_ints: Result<Vec<_>, _> = partition_ids
-            .iter()
-            .map(|id| id.as_str().parse::<u64>().map(|n| (id, n)))
-            .collect();
-
-        let partitioner = if let Ok(mut ids_and_ints) = ids_and_ints {
-            ids_and_ints.sort_by_key(|x| x.1);
-            Partitioner::new_with_hasher(
-                ids_and_ints.into_iter().map(|(p, _)| p.clone()).collect(),
-                build_hasher,
-            )
-        } else {
-            Partitioner::new_sorted_with_hasher(partition_ids, build_hasher)
-        };
-
-        Ok(partitioner)
+        Ok(create_sorted_partitioner(partitions, build_hasher))
     }
 
+    /// Returns the partition that matches the given key
     fn partition_for_key<H>(&self, partition_key: &H) -> &PartitionId
     where
         H: Hash,
@@ -114,24 +152,30 @@ where
         partition_for_hash(&self.partitions, hasher.finish())
     }
 
-    pub fn partitions(&self) -> &[PartitionId] {
-        &self.partitions
-    }
-
+    /// Determines and assigns partitions
     pub fn assign<E: PartitionKeyExtractable + PartitionAssignable>(self, event: &mut E) {
         let key = event.partition_key();
         let partition = self.partition_for_key(&key);
         event.assign_partition(partition);
     }
+
+    /// Returns the partitions as used by the `Partitioner`
+    pub fn partitions(&self) -> &[PartitionId] {
+        &self.partitions
+    }
 }
 
+/// Can return a key for manual partitioning
 pub trait PartitionKeyExtractable {
     type Key: Hash;
 
+    /// Returns the key for partitioning
     fn partition_key(&self) -> Self::Key;
 }
 
+/// Can be assigned a partition
 pub trait PartitionAssignable {
+    /// Assign a partition.
     fn assign_partition(&mut self, partition: &PartitionId);
 }
 
@@ -144,6 +188,27 @@ impl<D> PartitionAssignable for BusinessEventPub<D> {
 impl<D> PartitionAssignable for DataChangeEventPub<D> {
     fn assign_partition(&mut self, partition: &PartitionId) {
         self.metadata.partition = Some(partition.clone());
+    }
+}
+
+fn create_sorted_partitioner<B: BuildHasher + Clone>(
+    mut partitions: Vec<PartitionId>,
+    build_hasher: B,
+) -> Partitioner<B> {
+    let ids_and_ints: Result<Vec<_>, _> = partitions
+        .iter()
+        .map(|id| id.as_str().parse::<u64>().map(|n| (id, n)))
+        .collect();
+
+    if let Ok(mut ids_and_ints) = ids_and_ints {
+        ids_and_ints.sort_by_key(|x| x.1);
+        Partitioner::new_with_hasher(
+            ids_and_ints.into_iter().map(|(p, _)| p.clone()).collect(),
+            build_hasher,
+        )
+    } else {
+        partitions.sort();
+        Partitioner::new_sorted_with_hasher(partitions, build_hasher)
     }
 }
 
