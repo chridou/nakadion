4f2b57afb76bf332a27b963cf5b1518cb5a4378a
cdouven <christian.douven@zalando.de>
2020-05-05 00:28:45 +0200

    fix partition activity tracking

--- nakadion/src/internals/controller/partition_tracker.rs
+++ nakadion/src/internals/controller/partition_tracker.rs
@@ -25,37 +25,41 @@ impl PartitionTracker {
         }
     }
 
+    /// Call when something was received
     pub fn activity(&mut self, partition: &EventTypePartition) {
         let now = Instant::now();
         if let Some(entry) = self.partitions.get_mut(partition) {
-            if entry.activity(now) {
+            if let Some(was_inactive_for) = entry.activity(now) {
                 self.logger.info(format_args!(
-                    "Partition {} is active again",
-                    partition
+                    "Event type partition {} is active again after {:?} of inactivity",
+                    partition, was_inactive_for,
                 ));
                 self.instrumentation.controller_partition_activated();
             }
         } else {
             let entry = Entry {
-                activated_at: Some(now),
+                state: PartitionActivationState::ActiveSince(now),
                 last_activity_at: now,
             };
             self.partitions.insert(partition.clone(), entry);
-            self.logger
-                .info(format_args!("New active partition {}", partition));
+            self.logger.info(format_args!(
+                "New active event type partition {}",
+                partition
+            ));
             self.instrumentation.controller_partition_activated();
         }
     }
 
+    /// Call on tick to check inactivity
     pub fn check_for_inactivity(&mut self, now: Instant) {
         for (partition, entry) in self.partitions.iter_mut() {
-            if let Some(activated_at) = entry.check_for_inactivity(now, self.inactivity_after) {
+            if let Some(was_active_for) = entry.check_for_inactivity(now, self.inactivity_after) {
                 self.logger.info(format_args!(
-                    "Partition {} became inactive",
-                    partition
+                    "Partition {} became inactive after {:?}",
+                    partition, was_active_for
                 ));
                 self.instrumentation
-                    .controller_partition_deactivated(activated_at.elapsed())
+                    .controller_partition_deactivated(was_active_for)
             }
         }
     }
@@ -63,50 +67,59 @@ impl PartitionTracker {
 
 impl Drop for PartitionTracker {
     fn drop(&mut self) {
-        for activated_at in self
-            .partitions
-            .iter()
-            .filter_map(|(_, entry)| entry.activated_at)
-        {
-            self.instrumentation
-                .controller_partition_deactivated(activated_at.elapsed())
+        for entry in self.partitions.iter().map(|(_, entry)| entry) {
+            match entry.state {
+                PartitionActivationState::ActiveSince(when) => self
+                    .instrumentation
+                    .controller_partition_deactivated(when.elapsed()),
+                PartitionActivationState::InactiveSince(_when) => {}
+            }
         }
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+enum PartitionActivationState {
+    ActiveSince(Instant),
+    InactiveSince(Instant),
+}
+
 struct Entry {
-    activated_at: Option<Instant>,
+    state: PartitionActivationState,
     last_activity_at: Instant,
 }
 
 impl Entry {
-    /// Returns true if the partition was reactivated
-    pub fn activity(&mut self, now: Instant) -> bool {
-        let was_inactive = self.activated_at.is_none();
-        if was_inactive {
-            self.activated_at = Some(now);
+    /// Returns Some(inactive for) if the partition was reactivated
+    pub fn activity(&mut self, now: Instant) -> Option<Duration> {
+        match self.state {
+            PartitionActivationState::ActiveSince(_) => {
+                self.last_activity_at = now;
+                None
+            }
+            PartitionActivationState::InactiveSince(when) => {
+                self.state = PartitionActivationState::ActiveSince(now);
+                Some(when.elapsed())
+            }
         }
-        self.last_activity_at = now;
-
-        was_inactive
     }
 
-    /// Returns `Some(Instant)` if the partition was deactivated.
-    /// The timestamp is when the partition was activated.
+    /// Returns `Some(active_for)` if the partition was deactivated.
     pub fn check_for_inactivity(
         &mut self,
         now: Instant,
         inactive_after: Duration,
-    ) -> Option<Instant> {
-        if let Some(activated_at) = self.activated_at {
-            if activated_at + inactive_after < now {
-                self.activated_at = None;
-                Some(activated_at)
-            } else {
-                None
+    ) -> Option<Duration> {
+        match self.state {
+            PartitionActivationState::ActiveSince(when) => {
+                if self.last_activity_at + inactive_after < now {
+                    self.state = PartitionActivationState::InactiveSince(now);
+                    Some(when.elapsed())
+                } else {
+                    None
+                }
             }
-        } else {
-            None
+            PartitionActivationState::InactiveSince(_) => None,
         }
     }
 }
