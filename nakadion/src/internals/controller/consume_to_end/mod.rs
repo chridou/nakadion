de1463c52229fc046d9f28b2d45a2128a4c89b49
cdouven <christian.douven@zalando.de>
2020-05-10 19:07:26 +0200

    renamed consumedata to controllerstate

--- nakadion/src/internals/controller/consume_to_end/mod.rs
+++ nakadion/src/internals/controller/consume_to_end/mod.rs
@@ -35,7 +35,7 @@ where
 {
     let instrumentation = stream_state.instrumentation();
 
-    let mut consume_data = ConsumeData::new(stream_state.clone());
+    let mut controller_state = ControllerState::new(stream_state.clone());
 
     let mut stream = stream.boxed();
 
@@ -58,7 +58,7 @@ where
                 if let Err(err) = handle_batch_line(
                     batch_line,
                     &stream_state,
-                    &mut consume_data,
+                    &mut controller_state,
                     &mut dispatcher_sink,
                 )
                 .await
@@ -73,7 +73,7 @@ where
                 if let Err(err) = handle_tick(
                     tick_timestamp,
                     &stream_state,
-                    &mut consume_data,
+                    &mut controller_state,
                     &mut dispatcher_sink,
                 )
                 .await
@@ -111,7 +111,7 @@ where
         active_dispatcher,
         dispatcher_sink,
         stream_state.clone(),
-        &consume_data,
+        &controller_state,
     )
     .await;
 
@@ -120,7 +120,7 @@ where
             stream_state.info(format_args!(
                 "Streaming infrastructure shut down after {:?}. Will reconnect \
                 if consumer was not requested to stop.",
-                consume_data.stream_started_at.elapsed(),
+                controller_state.stream_started_at.elapsed(),
             ));
             Ok(sleeping_dispatcher)
         }
@@ -128,7 +128,7 @@ where
             stream_state.warn(format_args!(
                 "Streaming infrastructure shut down after {:?} with an error \
                 (causes consumer abort).",
-                consume_data.stream_started_at.elapsed(),
+                controller_state.stream_started_at.elapsed(),
             ));
             Err(shutdown_err)
         }
@@ -136,7 +136,7 @@ where
             stream_state.warn(format_args!(
                 "Streaming infrastructure shut down after {:?} because there \
                 was an unrecoverable error (causes consumer abort).",
-                consume_data.stream_started_at.elapsed(),
+                controller_state.stream_started_at.elapsed(),
             ));
             Err(loop_err)
         }
@@ -146,7 +146,7 @@ where
             was an unrecoverable error (causes consumer abort). Also the shutdown of \
             the infrastructure caused an error. Error which caused the streaming \
             infrastructure to shut down: {}",
-                consume_data.stream_started_at.elapsed(),
+                controller_state.stream_started_at.elapsed(),
                 loop_err,
             ));
             // We return the shutdown error because this is most probably an
@@ -159,15 +159,15 @@ where
 async fn handle_batch_line(
     batch_line: BatchLine,
     stream_state: &StreamState,
-    consume_data: &mut ConsumeData,
+    controller_state: &mut ControllerState,
     dispatcher_sink: &mut UnboundedSender<DispatcherMessage>,
 ) -> Result<(), Error> {
     let frame_received_at = batch_line.received_at();
     let now = Instant::now();
-    consume_data.last_frame_received_at = now;
+    controller_state.last_frame_received_at = now;
 
     let event_type_partition = batch_line.to_event_type_partition();
-    consume_data
+    controller_state
         .partition_tracker
         .activity(&event_type_partition);
 
@@ -191,7 +191,7 @@ async fn handle_batch_line(
         ));*/
         Ok(())
     } else {
-        consume_data.last_events_received_at = now;
+        controller_state.last_events_received_at = now;
         let bytes = batch_line.bytes().len();
         stream_state
             .instrumentation
@@ -209,7 +209,7 @@ async fn handle_batch_line(
             stream_state
                 .instrumentation
                 .consumer_batches_in_flight_inc();
-            consume_data.batches_sent_to_dispatcher += 1;
+            controller_state.batches_sent_to_dispatcher += 1;
             Ok(())
         }
     }
@@ -218,33 +218,33 @@ async fn handle_batch_line(
 async fn handle_tick(
     tick_timestamp: Instant,
     stream_state: &StreamState,
-    consume_data: &mut ConsumeData,
+    controller_state: &mut ControllerState,
     dispatcher_sink: &mut UnboundedSender<DispatcherMessage>,
 ) -> Result<(), Error> {
-    if consume_data.stream_dead_policy.is_stream_dead(
-        consume_data.last_frame_received_at,
-        consume_data.last_events_received_at,
+    if controller_state.stream_dead_policy.is_stream_dead(
+        controller_state.last_frame_received_at,
+        controller_state.last_events_received_at,
     ) {
         return Err(Error::new("The stream is dead boys..."));
     }
 
-    let elapsed = consume_data.last_frame_received_at.elapsed();
-    if elapsed >= consume_data.warn_no_frames {
+    let elapsed = controller_state.last_frame_received_at.elapsed();
+    if elapsed >= controller_state.warn_no_frames {
         stream_state.warn(format_args!("No frames for {:?}.", elapsed));
         stream_state
             .instrumentation()
             .controller_no_frames_warning(elapsed);
     }
 
-    let elapsed = consume_data.last_events_received_at.elapsed();
-    if elapsed >= consume_data.warn_no_events {
+    let elapsed = controller_state.last_events_received_at.elapsed();
+    if elapsed >= controller_state.warn_no_events {
         stream_state.warn(format_args!("No events received for {:?}.", elapsed));
         stream_state
             .instrumentation()
             .controller_no_events_warning(elapsed);
     }
 
-    consume_data
+    controller_state
         .partition_tracker
         .check_for_inactivity(Instant::now());
 
@@ -259,7 +259,7 @@ async fn shutdown<C, S>(
     active_dispatcher: ActiveDispatcher<'static, C>,
     dispatcher_sink: UnboundedSender<DispatcherMessage>,
     stream_state: StreamState,
-    consume_data: &ConsumeData,
+    controller_state: &ControllerState,
 ) -> Result<SleepingDispatcher<C>, ConsumerError>
 where
     C: SubscriptionCommitApi + Clone + Send + Sync + 'static,
@@ -272,7 +272,7 @@ where
 
     stream_state.debug(format_args!(
         "Streaming ending after {:?}. Waiting for stream infrastructure to shut down.",
-        consume_data.stream_started_at.elapsed()
+        controller_state.stream_started_at.elapsed()
     ));
 
     // Wait for the infrastructure to completely shut down before making further connect attempts for
@@ -283,7 +283,7 @@ where
             payload: sleeping_dispatcher,
         }) => (
             Ok(sleeping_dispatcher),
-            Some(consume_data.batches_sent_to_dispatcher - processed_batches),
+            Some(controller_state.batches_sent_to_dispatcher - processed_batches),
         ),
         Err(EnrichedErr {
             processed_batches,
@@ -293,7 +293,7 @@ where
             if let Some(processed_batches) = processed_batches {
                 (
                     Err(err),
-                    Some(consume_data.batches_sent_to_dispatcher - processed_batches),
+                    Some(controller_state.batches_sent_to_dispatcher - processed_batches),
                 )
             } else {
                 (Err(err), None)
@@ -317,7 +317,7 @@ where
     result
 }
 
-struct ConsumeData {
+struct ControllerState {
     /// We might want to abort if we do not receive data from Nakadi in time
     /// This is basically to prevents us from being locked in a dead stream.
     stream_dead_policy: StreamDeadPolicy,
@@ -332,7 +332,7 @@ struct ConsumeData {
     partition_tracker: PartitionTracker,
 }
 
-impl ConsumeData {
+impl ControllerState {
     pub fn new(stream_state: StreamState) -> Self {
         let now = Instant::now();
         Self {
